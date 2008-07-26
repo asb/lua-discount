6ca3fa825466256b5ef58e24142d551f1439b717
A.S. Bradbury <asb@asbradbury.org>
2008-07-26 13:07:44 +0000

    Add support for specifying options as extra arguments to discount()
    Supports specifying any of the options provided by discount. e.g.
    discount(str, "nolinks", "nopants")

--- ldiscount.c
+++ ldiscount.c
@@ -20,22 +20,49 @@ static cookie_io_functions_t fakehandle_functions = {
   (cookie_close_function_t*)NULL
 };
 
+static const char *const discount_opts[] = {
+  "nolinks",
+  "noimages",
+  "nopants",
+  "tagtext",
+  "noext",
+  "cdata",
+  "embed",
+  NULL
+};
+
+static const int discount_opts_codes[] = {
+  MKD_NOLINKS,
+  MKD_NOIMAGE,
+  MKD_NOPANTS,
+  MKD_TAGTEXT,
+  MKD_NO_EXT,
+  MKD_CDATA,
+  MKD_EMBED
+};
+
 static int ldiscount(lua_State *L) {
   size_t len;
   const char *str = luaL_checklstring(L, 1, &len);
-  int flags = MKD_TABSTOP | MKD_NOHEADER;
+  int flags = 0;
+  int num_args = lua_gettop(L);
+  int i;
+  for (i = 2; i <= num_args; i++) {
+    int opt_index = luaL_checkoption(L, i, NULL, discount_opts);
+    flags |= discount_opts_codes[opt_index];
+  }
 
   luaL_Buffer b;
   luaL_buffinit(L, &b);
 
   FILE *fh = fopencookie((void*)&b, "w", fakehandle_functions);
 
-  MMIOT *doc = mkd_string(str, len, flags);
+  MMIOT *doc = mkd_string(str, len, MKD_TABSTOP|MKD_NOHEADER);
   int ret = markdown(doc, fh, flags);
   fclose(fh);
+  luaL_pushresult(&b);
   if (ret < 0)
     return luaL_error(L, "error in markdown conversion");
-  luaL_pushresult(&b);
   return 1;
 }
 
