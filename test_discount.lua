f68c5a07d675a981c1a4f59985ebd83cc7e46dac
A.S. Bradbury <asb@asbradbury.org>
2008-09-03 21:49:40 +0000

    improve test coverage

--- test_discount.lua
+++ test_discount.lua
@@ -16,6 +16,22 @@ function test_relaxed_emphasis()
   assert_equal(expected_out, discount(input))
 end
 
+function test_nolinks()
+  assert_equal("<p>[example](http://example.com)</p>\n", discount("[example](http://example.com)", "nolinks"))
+  assert_equal('<p>&lt;a href="http://example.com">example</a></p>\n',
+      discount('<a href="http://example.com">example</a>', "nolinks"))
+end
+
+function test_noimages()
+  assert_equal("<p>![example](example.png)</p>\n", discount("![example](example.png)", "noimages"))
+  assert_equal('<p>&lt;img src="example.png"/></p>\n', discount('<img src="example.png"/>', "noimages"))
+end
+
+function test_nopants()
+  assert_equal('<p>&ldquo;quote&rdquo;</p>\n', discount('"quote"'))
+  assert_equal('<p>"quote"</p>\n', discount('"quote"', "nopants"))
+end
+
 function test_nohtml()
   local expected = "<p>This should &lt;em>not&lt;/em> be allowed</p>\n"
   assert_equal(expected, discount("This should <em>not</em> be allowed", "nohtml"))
