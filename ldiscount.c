b4d0961288fd47635af8dc40802724b1d8d6304a
A.S. Bradbury <asb@asbradbury.org>
2010-03-06 21:16:23 +0000

    register a discount table, but make a functable for backward compatability

--- ldiscount.c
+++ ldiscount.c
@@ -67,7 +67,27 @@ static int ldiscount(lua_State *L) {
   }
 }
 
+static int ldiscount__call(lua_State *L) {
+  luaL_checktype(L, 1, LUA_TTABLE);
+  lua_remove(L, 1);
+  return ldiscount(L);
+}
+
+static const struct luaL_reg ldiscount_meta[] = {
+  {"__call", ldiscount__call},
+  {NULL, NULL}
+};
+
+static const struct luaL_reg ldiscount_funcs[] = {
+  {"to_html", ldiscount},
+  {NULL, NULL}
+};
+
 LUALIB_API int luaopen_discount(lua_State *L) {
-  lua_pushcfunction(L, ldiscount);
+  // Give the discount table a metatable
+  luaL_register(L, "discount", ldiscount_funcs);
+  lua_newtable(L);
+  luaL_register(L, NULL, ldiscount_meta);
+  lua_setmetatable(L, -2);
   return 1;
 }
